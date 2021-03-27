609e67f5fd59f47bf2c012e26f594b05d897d510
Brandon Haynes <brandonhaynes100@gmail.com>
2021-03-27 12:23:48 -0700

    in progress SVG integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNav from "./components/MyNav"
 import MyJumbo from "./components/MyJumbo"
 import ProjectsContainer from "./components/ProjectsContainer"
 import Contact from "./components/Contact"
+
+// import React from 'react';
+import {ReactComponent as ReactLogo} from './logo.svg';
+
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
       <MyJumbo></MyJumbo>
       <ProjectsContainer></ProjectsContainer>
       <Contact></Contact>
+      <ReactLogo />
     </>
   );
 }
